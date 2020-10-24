1c658a38f1ca33014319685f84579ae36acd7df7
Oleg Bogomolov <smmac072@gmail.com>
2020-10-24 21:58:51 +0300

    update
    вернул старый асм, пару фиксов в main.c

--- src/sort.s
+++ src/sort.s
@@ -1,13 +1,13 @@
 	.file	"sort.c"
 	.text
-	.p2align 4,,15
-	.globl	sort
-	.type	sort, @function
-sort:
+	.p2align 4
+	.globl	sorting
+	.type	sorting, @function
+sorting:
 .LFB0:
 	.cfi_startproc
-	movl	(%rsi), %edx
-	subl	$1, %edx
+	endbr64
+	leal	-1(%rsi), %edx
 	testl	%edx, %edx
 	jle	.L1
 	leaq	8(%rdi), %r9
@@ -16,8 +16,8 @@ sort:
 .L3:
 	subl	$1, %edx
 	movq	%rdi, %rax
-	leaq	(%r9,%rdx,8), %r8
-	movq	%rdx, %rsi
+	movq	%rdx, %r8
+	leaq	(%r9,%rdx,8), %rsi
 	.p2align 4,,10
 	.p2align 3
 .L5:
@@ -29,15 +29,31 @@ sort:
 	movq	%rdx, 8(%rax)
 .L4:
 	addq	$8, %rax
-	cmpq	%r8, %rax
+	cmpq	%rsi, %rax
 	jne	.L5
-	testl	%esi, %esi
-	movl	%esi, %edx
+	movl	%r8d, %edx
+	testl	%r8d, %r8d
 	jne	.L3
 .L1:
-	rep ret
+	ret
 	.cfi_endproc
 .LFE0:
-	.size	sort, .-sort
-	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.size	sorting, .-sorting
+	.ident	"GCC: (Ubuntu 9.3.0-10ubuntu2) 9.3.0"
 	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
